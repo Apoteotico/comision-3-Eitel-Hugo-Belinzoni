ebe8ea49f0f1a8f78303d22257466662d30faefc
Apoteotico <eitel.trabajo@gmail.com>
2023-12-01 23:41:41 -0300

    haciendo el frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,16 +8,20 @@ import authRouter from "../src/routes/auth.routes.js";
 import PostRouter from "./routes/post.routes.js";
 import CommentRouter from "./routes/comment.routes.js"; 
 
+
 const app = express();
 
+//Middlewares
 app.use(express.json()); 
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(cors());
+
+//Rutas
 app.use("/", indexRoutes);
 app.use("/api", authRouter);
-app.use("/api", PostRouter);
-app.use("/api", CommentRouter); 
-app.use(cors());
+app.use("/api/posts", PostRouter);
+app.use("/api/comments", CommentRouter); 
 
 export default app;
